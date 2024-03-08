9ee35c4903b54554b4b4f98331667bc593f9296c
Kadir Mete <abdulkadir.mete@bil.omu.edu.tr>
2024-03-08 11:09:20 +0300

    Create vim-auto-install.sh

---
+++ vim-auto-install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Install Vim Plug
+echo "Installing Vim Plug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Create or overwrite .vimrc
+echo "Creating .vimrc..."
+cat > ~/.vimrc << EOF
+call plug#begin()
+Plug 'preservim/nerdtree'
+Plug 'vimwiki/vimwiki'
+Plug 'junegunn/goyo.vim'
+Plug 'farmergreg/vim-lastplace'
+Plug 'vim-airline/vim-airline'
+Plug 'tpope/vim-commentary'
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+Plug 'dmerejkowsky/vim-ale'
+Plug 'ervandew/supertab'
+call plug#end()
+
+map <F2> :NERDTreeToggle<CR>
+inoremap jj <Esc>
+nnoremap <C-p> :Files<Cr>
+set noswapfile
+EOF
+
+# Reload .vimrc
+echo "Reloading .vimrc..."
+vim -c ":so $MYVIMRC" -c ":PlugInstall" -c ":q" -c ":q"
+
+# Install Git (assuming yum package manager)
+# echo "Installing Git..."
+# sudo yum install -y git
+
+# Install plugins
+echo "Installing Vim plugins..."
+vim -c ":PlugInstall" -c ":q" -c ":q"
+
+echo "Setup complete!"
